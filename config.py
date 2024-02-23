94860e024f27f27a8b026f1d1268ee9cf0509ea5
TeamUniversTU <138324953+TeamUniverseTU@users.noreply.github.com>
2024-02-23 21:50:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,7 +59,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "​❗️ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ \n\nɪ ᴀᴍ ᴏɴʟʏ ᴀ ꜰɪʟᴇ-ꜱʜᴀʀɪɴɢ ʙᴏᴛ \n\n\ ᴄʀᴇᴀᴛᴇᴅ ʙʏ - @TeamUniverseOffical"
+USER_REPLY_TEXT = "​❗️ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ \n\nɪ ᴀᴍ ᴏɴʟʏ ᴀ ꜰɪʟᴇ-ꜱʜᴀʀɪɴɢ ʙᴏᴛ \n\nᴄʀᴇᴀᴛᴇᴅ ʙʏ - @TeamUniverseOffical"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(5691800418)
